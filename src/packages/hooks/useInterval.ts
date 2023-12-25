bc6d91531f276e8b5085e06577228ab2111563a6
wade-portkey <codingwizard@portkey.finance>
2023-12-25 16:28:16 +0800

    fix: 🐛 remove web3 and react-use package

--- src/packages/hooks/useInterval.ts
+++ src/packages/hooks/useInterval.ts
@@ -1,5 +1,5 @@
 import { DependencyList, useRef, useCallback, useMemo } from 'react';
-import { useDeepCompareEffect } from 'react-use';
+// import { useDeepCompareEffect } from 'react-use';
 import { useLatestRef } from './';
 
 const useInterval = (callback: () => void, delay?: number | null, deps?: DependencyList) => {
@@ -13,12 +13,12 @@ const useInterval = (callback: () => void, delay?: number | null, deps?: Depende
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [delay, ...(deps || [])]);
 
-  useDeepCompareEffect(() => {
-    if (delay !== null) startInterval();
-    return () => {
-      intervalRef.current && clearInterval(intervalRef.current as unknown as number);
-    };
-  }, [delay, deps, startInterval]);
+  // useDeepCompareEffect(() => {
+  //   if (delay !== null) startInterval();
+  //   return () => {
+  //     intervalRef.current && clearInterval(intervalRef.current as unknown as number);
+  //   };
+  // }, [delay, deps, startInterval]);
 
   return useMemo(
     () => ({
