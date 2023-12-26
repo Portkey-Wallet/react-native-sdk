0ad6b8e695bb7622dfc2c1b982f84c7515862510
wade-portkey <codingwizard@portkey.finance>
2023-12-26 11:27:40 +0800

    fix: 🐛 fix QRCode useInterval not work error

--- src/packages/hooks/useInterval.ts
+++ src/packages/hooks/useInterval.ts
@@ -1,5 +1,25 @@
-import { DependencyList, useRef, useCallback, useMemo } from 'react';
+import { DependencyList, EffectCallback, useRef, useCallback, useMemo } from 'react';
 import { useLatestRef } from './';
+import useCustomCompareEffect from './useCustomCompareEffect';
+import isDeepEqual from 'fast-deep-equal/react';
+
+const isPrimitive = (val: any) => val !== Object(val);
+
+const useDeepCompareEffect = (effect: EffectCallback, deps: DependencyList) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (!(deps instanceof Array) || !deps.length) {
+      console.warn('`useDeepCompareEffect` should not be used with no dependencies. Use React.useEffect instead.');
+    }
+
+    if (deps.every(isPrimitive)) {
+      console.warn(
+        '`useDeepCompareEffect` should not be used with dependencies that are all primitive values. Use React.useEffect instead.'
+      );
+    }
+  }
+
+  useCustomCompareEffect(effect, deps, isDeepEqual);
+};
 
 const useInterval = (callback: () => void, delay?: number | null, deps?: DependencyList) => {
   const intervalRef = useRef<NodeJS.Timer | number>();
@@ -11,6 +31,13 @@ const useInterval = (callback: () => void, delay?: number | null, deps?: Depende
     intervalRef.current = setInterval(() => savedCallback.current?.(), delay || 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [delay, ...(deps || [])]);
+
+  useDeepCompareEffect(() => {
+    if (delay !== null) startInterval();
+    return () => {
+      intervalRef.current && clearInterval(intervalRef.current as unknown as number);
+    };
+  }, [delay, deps, startInterval]);
 
   return useMemo(
     () => ({
