5b646d396902ffa54afe58f3bbbade0084034cca
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-12-29 17:35:51 +0800

    feat: update send home page

--- src/components/SendButton/index.tsx
+++ src/components/SendButton/index.tsx
@@ -1,36 +1,60 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useMemo } from 'react';
 import Svg from 'components/Svg';
-import { dashBoardBtnStyle, innerPageStyles } from './style';
-// import navigationService from 'utils/navigationService';
-import { TokenItemShowType } from 'packages/types/types-ca/token';
-import { View, TouchableOpacity } from 'react-native';
+import { commonButtonStyle } from './style';
+import { View, StyleProp, ViewProps } from 'react-native';
 import { TextM } from 'components/CommonText';
 import { useLanguage } from 'i18n/hooks';
 import { pTd } from 'utils/unit';
 import GStyles from 'assets/theme/GStyles';
-import CommonToast from 'components/CommonToast';
+import Touchable from 'components/Touchable';
+import { TokenItemShowType } from 'packages/types/types-eoa/token';
+import useBaseContainer from 'model/container/UseBaseContainer';
+import { PortkeyEntries } from 'config/entries';
+import { IToSendHomeParamsType } from 'packages/types/types-ca/routeParams';
 
 interface SendButtonType {
-  themeType?: 'dashBoard' | 'innerPage';
+  themeType?: 'dashBoard' | 'tokenInnerPage' | 'nftInnerPage';
   sentToken?: TokenItemShowType;
+  wrapStyle?: StyleProp<ViewProps>;
 }
 
 const SendButton = (props: SendButtonType) => {
-  const { themeType = 'dashBoard' } = props;
-  const styles = themeType === 'dashBoard' ? dashBoardBtnStyle : innerPageStyles;
-
   const { t } = useLanguage();
+  const { themeType = 'dashBoard', sentToken, wrapStyle = {} } = props;
+
+  const buttonTitleStyle = useMemo(
+    () =>
+      themeType === 'dashBoard'
+        ? commonButtonStyle.dashBoardTitleColorStyle
+        : commonButtonStyle.innerPageTitleColorStyle,
+    [themeType],
+  );
+
+  const entryName = useMemo(() => {
+    if (themeType === 'dashBoard') return PortkeyEntries.ASSETS_HOME_ENTRY;
+    if (themeType === 'tokenInnerPage') return PortkeyEntries.TOKEN_DETAIL_ENTRY;
+    return PortkeyEntries.NFT_DETAIL_ENTRY;
+  }, [themeType]);
+
+  const { navigateTo } = useBaseContainer({
+    entryName,
+  });
+
+  const onPressButton = useCallback(() => {
+    if (themeType !== 'dashBoard') {
+      navigateTo<IToSendHomeParamsType>(PortkeyEntries.SEND_TOKEN_HOME_ENTRY, {
+        params: {},
+      });
+    }
+    // AssetsOverlay.showAssetList();
+  }, [sentToken, themeType]);
 
   return (
-    <View style={styles.buttonWrap}>
-      <TouchableOpacity
-        style={[styles.iconWrapStyle, GStyles.alignCenter]}
-        onPress={async () => {
-          CommonToast.fail('Send is not available by now');
-        }}>
+    <View style={[commonButtonStyle.buttonWrap, wrapStyle]}>
+      <Touchable style={[commonButtonStyle.iconWrapStyle, GStyles.alignCenter, wrapStyle]} onPress={onPressButton}>
         <Svg icon={themeType === 'dashBoard' ? 'send' : 'send1'} size={pTd(46)} />
-      </TouchableOpacity>
-      <TextM style={styles.titleStyle}>{t('Send')}</TextM>
+      </Touchable>
+      <TextM style={[commonButtonStyle.commonTitleStyle, buttonTitleStyle]}>{t('Send')}</TextM>
     </View>
   );
 };
