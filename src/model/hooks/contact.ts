6fefc851e452d3ac5e04f80dbc1f52af74b16c9d
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2024-01-10 17:14:19 +0800

    feat: avoid duplicate recent records

--- src/model/hooks/contact.ts
+++ src/model/hooks/contact.ts
@@ -2,7 +2,7 @@ import useEffectOnce from 'hooks/useEffectOnce';
 import { getUnlockedWallet } from 'model/wallet';
 import { NetworkController } from 'network/controller';
 import { GetContractListApiType, RecentTransactionResponse } from 'network/dto/query';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 export const useContact = () => {
   const [contact, setContact] = useState<GetContractListApiType>({
@@ -16,40 +16,51 @@ export const useContact = () => {
   return contact;
 };
 
-export const useRecent = () => {
+export const useRecent = (removeDuplicateResult = true) => {
   const [recent, setRecent] = useState<RecentTransactionResponse & RefreshHandler>({
     totalRecordCount: 0,
     data: [],
     skipCount: 0,
   });
-  const loadMoreRecent = async (reset = false) => {
-    const { skipCount, data: originData, totalRecordCount: pastTotalRecordCount } = recent;
-    if (pastTotalRecordCount > 0 && originData.length >= pastTotalRecordCount) return;
-    const { multiCaAddresses } = await getUnlockedWallet({ getMultiCaAddresses: true });
-    const result = await NetworkController.getRecentTransactionInfo({
-      caAddressInfos: Object.entries(multiCaAddresses).map(([chainId, caAddress]) => ({
-        chainId,
-        caAddress,
-      })),
-      skipCount: reset ? 0 : skipCount,
-      maxResultCount: 100,
-    });
-    const { data, totalRecordCount } = result;
-    if (reset) {
-      setRecent({
-        totalRecordCount,
-        data,
-        skipCount: data.length,
+  const filteredRecent = useMemo(() => {
+    return {
+      ...recent,
+      data: removeDuplicateResult
+        ? recent.data.filter((item, index, array) => array.findIndex(i => i.address === item.address) === index)
+        : recent.data,
+    };
+  }, [recent, removeDuplicateResult]);
+  const loadMoreRecent = useCallback(
+    async (reset = false) => {
+      const { skipCount, data: originData, totalRecordCount: pastTotalRecordCount } = recent;
+      if (pastTotalRecordCount > 0 && originData.length >= pastTotalRecordCount) return;
+      const { multiCaAddresses } = await getUnlockedWallet({ getMultiCaAddresses: true });
+      const result = await NetworkController.getRecentTransactionInfo({
+        caAddressInfos: Object.entries(multiCaAddresses).map(([chainId, caAddress]) => ({
+          chainId,
+          caAddress,
+        })),
+        skipCount: reset ? 0 : skipCount,
+        maxResultCount: 100,
       });
-    } else {
-      setRecent({
-        data: originData.concat(data),
-        totalRecordCount,
-        skipCount: skipCount + data.length,
-      });
-    }
-  };
-  return { recent, loadMoreRecent };
+      const { data, totalRecordCount } = result;
+      if (reset) {
+        setRecent({
+          totalRecordCount,
+          data,
+          skipCount: data.length,
+        });
+      } else {
+        setRecent({
+          data: originData.concat(data),
+          totalRecordCount,
+          skipCount: skipCount + data.length,
+        });
+      }
+    },
+    [recent],
+  );
+  return { recent: filteredRecent, loadMoreRecent };
 };
 
 export interface RefreshHandler {
