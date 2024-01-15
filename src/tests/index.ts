8473b27a9b0d8bed0fcaa17f171a7c0c92dfd36e
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2024-01-15 11:47:35 +0800

    fix: path issues

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -2,6 +2,8 @@ import BatchedBridge from 'react-native/Libraries/BatchedBridge/BatchedBridge';
 import { TestModule } from './TestModule';
 import { JSModuleIdentifier } from 'service/JsModules/BatchedBridges';
 
-if (__DEV__) {
-  BatchedBridge.registerCallableModule(JSModuleIdentifier.TEST_MODULE, TestModule);
-}
+export const registerTestModule = () => {
+  if (__DEV__) {
+    BatchedBridge.registerCallableModule(JSModuleIdentifier.TEST_MODULE, TestModule);
+  }
+};
