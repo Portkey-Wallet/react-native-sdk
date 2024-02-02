d221f3a3ef9535700c1d7ea9aca9d2b950c5b281
wade-portkey <codingwizard@portkey.finance>
2024-02-02 17:48:04 +0800

    feat: ramp debug sdk api request

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,12 +1,13 @@
-import { ServiceInit } from 'packages/api/server/config';
 import { BaseConfig, RequestConfig } from 'packages/api/types';
 import { NetworkController } from 'network/controller';
+import { IRampRequest } from 'packages/ramp';
 
-export class RnsdkService extends ServiceInit {
+class RnsdkService implements IRampRequest {
   send = async (base: BaseConfig, config?: RequestConfig | undefined): Promise<any> => {
-    const url = await this.parseUrl(config?.url);
+    const url = await NetworkController.parseUrl(config?.url ?? '');
     const method = (config?.method || 'GET') as 'GET' | 'POST';
-    return NetworkController.realExecute(url, method, config?.params, config?.headers);
+    const res = NetworkController.realExecute(url, method, config?.params, config?.headers);
+    return res;
   };
 }
 
