e2c4b4e602e8e34e9cbc2b866d51a8a7dbf6da71
wade-portkey <codingwizard@portkey.finance>
2024-02-05 10:35:27 +0800

    feat: ramp finish redux data flow

--- src/network/api.ts
+++ src/network/api.ts
@@ -6,7 +6,10 @@ class RnsdkService implements IRampRequest {
   send = async (base: BaseConfig, config?: RequestConfig | undefined): Promise<any> => {
     const url = await NetworkController.parseUrl(config?.url ?? '');
     const method = (config?.method || 'GET') as 'GET' | 'POST';
-    const res = NetworkController.realExecute(url, method, config?.params, config?.headers);
+    const res = await NetworkController.realExecute(url, method, config?.params, config?.headers);
+    if (res?.result) {
+      return res.result;
+    }
     return res;
   };
 }
