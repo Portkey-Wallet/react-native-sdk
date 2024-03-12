2bd9262af3ac6a2bd9752f861f5394d4c9442ab9
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-03-12 17:46:27 +0800

    fix: change const name

--- src/packages/constants/wallet.ts
+++ src/packages/constants/wallet.ts
@@ -1,2 +1,2 @@
 export const DefaultBIP44Path = "m/44'/1616'/0'/0/0";
-export const defaultViewPrivateKey = '6167c717e781099c8ee77cbf0c3f6e7c8315fc581eb7daa891c872c026359c84';
+export const DefaultViewPrivateKey = '6167c717e781099c8ee77cbf0c3f6e7c8315fc581eb7daa891c872c026359c84';
